33e1d0bd2875383c2ddf054574405b0f5d6937ee
Gary <yugao829@163.com>
2015-08-28 16:46:17 +0800

    Homework
    Homework

---
+++ app/src/main/java/com/example/yugao/homework_try1/Findwords.java
@@ -0,0 +1,23 @@
+package com.example.yugao.homework_try1;
+
+/**
+ * Created by yugao on 15/8/28.
+ */
+//public class Findwords {
+//
+//    public String readFile(String filename) throws IOException{
+//        String res = "";
+//        try{
+//            FileInputStream filein = openFileInput(filename);
+//            int length = filein.available();
+//            byte[] buffer = new byte[length];
+//            filein.read(buffer);
+//            res = EncodingUtils.getString(buffer,"UTF-8");
+//            filein.close();
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        return res;
+//    }
+//
+//}
